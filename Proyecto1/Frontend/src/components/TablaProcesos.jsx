175e0eac6aafb40492508c866a1f7ed5177845a7
RACSO <racsodraude0@gmail.com>
2024-06-13 01:14:02 -0600

    Mejorando diseño de la vista procesos
    Hora se ve en pantalla el ultimo proceso creado para una mejor gestion

--- Proyecto1/Frontend/src/components/TablaProcesos.jsx
+++ Proyecto1/Frontend/src/components/TablaProcesos.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'; // Importa React y hooks de estado y efectos
 import { useTable, useExpanded } from 'react-table'; // Importa funciones de react-table para crear tablas y manejar filas expandidas
 import { Table, Button, InputGroup, FormControl } from 'react-bootstrap'; // Importa componentes de react-bootstrap para la interfaz
-import '../components/Estilo.css'; // Importa estilos personalizados
+import '../styles/EstiloProcesos.css'; // Importa estilos personalizados
 
 function TablaProcesos() {
   // Define los estados del componente
@@ -11,6 +11,7 @@ function TablaProcesos() {
   const [info, setInfo] = useState({}); // Estado para la información adicional de los procesos
   const [pidToKill, setPidToKill] = useState(''); // Estado para almacenar el PID a eliminar
   const [searchBy, setSearchBy] = useState('name'); // Estado para determinar si se busca por nombre o por PID
+  const [lastCreatedPid, setLastCreatedPid] = useState(''); // Estado para almacenar el PID del último proceso creado
 
   // Efecto para cargar los datos de los procesos desde la API
   useEffect(() => {
@@ -85,36 +86,36 @@ function TablaProcesos() {
     fetch('http://192.168.122.195:8080/procesos/crear', {
       method: 'GET', // Cambia el método a GET según la definición de tu API
     })
-    
       .then(response => response.json())
       .then(data => {
         if (data.estado === 'creado') {
+          // Actualiza el estado con el PID del proceso creado
+          setLastCreatedPid(data.pid);
           // Mostrar alerta flotante para proceso creado exitosamente
-          showAlert(data.pid);
+          showAlert(`Proceso creado con PID: ${data.pid}`);
         } else {
           // Mostrar alerta flotante para error al crear proceso
-          showAlert(data.pid);
+          showAlert('Error al crear proceso');
         }
       })
       .catch(error => {
         console.error('Error creating process:', error);
         // Mostrar alerta flotante para error general
-        showAlert(data.pid);
+        showAlert('Error al crear proceso');
       });
   };
-  
+
   // Función para mostrar alertas flotantes
   const showAlert = (message) => {
     const alertContainer = document.createElement('div');
     alertContainer.className = 'alert-container';
     alertContainer.innerText = message;
     document.body.appendChild(alertContainer);
-  
+
     setTimeout(() => {
       alertContainer.remove();
     }, 3000); // Desaparece después de 3 segundos (ajustable según necesidad)
   };
-  
 
   const handleKillProcess = () => {
     fetch(`http://192.168.122.195:8080/procesos/eliminar/${pidToKill}`, {
@@ -126,18 +127,22 @@ function TablaProcesos() {
         if (data.estado === 'Eliminado') {
           // Mostrar alerta flotante para proceso eliminado exitosamente
           showAlert(`El proceso ${pidToKill} fue eliminado.`);
+          // Verificar si el proceso eliminado es el último creado y actualizar el estado
+          if (pidToKill === lastCreatedPid) {
+            setLastCreatedPid('');
+          }
         } else {
           // Mostrar alerta flotante para error al eliminar proceso
-          showAlert(data.pid);
+          showAlert('Error al eliminar proceso');
         }
       })
       .catch(error => {
         console.error('Error killing process:', error);
         // Mostrar alerta flotante para error general
-        showAlert(data.pid);
+        showAlert('Error al eliminar proceso');
       });
   };
-  
+
   // Prepara los datos para la tabla
   const data = React.useMemo(
     () =>
@@ -189,75 +194,73 @@ function TablaProcesos() {
   return (
     <div>
       <div className="title-container">
-    <h1>Tabla de Procesos</h1>
-    </div>
-    <table className="horizontal-table">
+        <h1>Tabla de Procesos</h1>
+      </div>
+      <table className="horizontal-table">
         <tbody>
-            <tr className="table-header">
-                <td>En ejecución (1)</td>
-                <td>Suspendidos (0)</td>
-                <td>Detenidos (128)</td>
-                <td>Zombies (1026)</td>
-                <td>Total</td>
-            </tr>
-            <tr className='subRow'>
-                <td>{info.en_ejecucion}</td>
-                <td>{info.suspendidos}</td>
-                <td>{info.detenidos}</td>
-                <td>{info.zombies}</td>
-                <td>{info.total}</td>
-            </tr>
+          <tr className="table-header">
+            <td>En ejecución (1)</td>
+            <td>Suspendidos (0)</td>
+            <td>Detenidos (128)</td>
+            <td>Zombies (1026)</td>
+            <td>Total</td>
+          </tr>
+          <tr className='subRow'>
+            <td>{info.en_ejecucion}</td>
+            <td>{info.suspendidos}</td>
+            <td>{info.detenidos}</td>
+            <td>{info.zombies}</td>
+            <td>{info.total}</td>
+          </tr>
         </tbody>
-    </table>
+      </table>
 
-
-<br />
-
-<Button variant="success" onClick={handleCreateProcess}>
-  <i className="bi bi-file-earmark-plus"></i> Crear Proceso
-</Button>
-
-<br /><br />
-
-<InputGroup className="mb-3">
-  <FormControl
-    placeholder="PID para eliminar"
-    value={pidToKill}
-    onChange={(e) => setPidToKill(e.target.value)}
-  />
-  <Button variant="danger" onClick={handleKillProcess}>
-    <i className="bi bi-file-earmark-x"></i> Eliminar Proceso
-  </Button>
-</InputGroup>
-
-<InputGroup className="mb-3">
-  {searchBy === 'name' ? (
-    <>
-      <FormControl
-        type="text"
-        placeholder="Buscar por nombre"
-        value={searchTerm}
-        onChange={handleSearchChange}
-      />
-      <Button onClick={handleSearchByChange}>
-        <i className="bi bi-search"></i> Buscar por PID
-      </Button>
-    </>
-  ) : (
-    <>
-      <FormControl
-        type="text"
-        placeholder="Buscar por PID"
-        value={searchPidTerm}
-        onChange={handleSearchPidChange}
-      />
-      <Button onClick={handleSearchByChange}>
-        <i className="bi bi-search"></i> Buscar por Nombre
-      </Button>
-    </>
-  )}
-</InputGroup>
-
+      <div>
+        <Table className="no-border-table">
+          <tbody>
+          <tr>
+            <td className="centered-cell">{lastCreatedPid ? `Último proceso creado: ${lastCreatedPid}` : 'Último proceso creado'}</td>
+            <td>
+              <Button variant="success" onClick={handleCreateProcess}>
+                <i className="bi bi-file-earmark-plus"></i> Crear Proceso
+              </Button>
+            </td>
+          </tr>
+            <tr>
+              <td>
+                <FormControl
+                  placeholder="PID para eliminar"
+                  value={pidToKill}
+                  onChange={(e) => setPidToKill(e.target.value)}
+                />
+              </td>
+              <td>
+                <Button variant="danger" onClick={handleKillProcess}>
+                  <i className="bi bi-file-earmark-x"></i> Eliminar Proceso
+                </Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <FormControl
+                  type="text"
+                  placeholder={searchBy === 'name' ? 'Buscar por nombre' : 'Buscar por PID'}
+                  value={searchBy === 'name' ? searchTerm : searchPidTerm}
+                  onChange={searchBy === 'name' ? handleSearchChange : handleSearchPidChange}
+                />
+              </td>
+              <td>
+                <Button
+                  style={{ fontSize: '14px', padding: '8px 12px', minHeight: '38px' }}
+                  onClick={handleSearchByChange}
+                >
+                  <i className="bi bi-search"></i> {searchBy === 'name' ? 'Buscar por PID' : 'Buscar por Nombre'}
+                </Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
 
       <Table {...getTableProps()} striped bordered hover>
         <thead>
